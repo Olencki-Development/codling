9c5ca375f1c4d34c362c6b288e4c67286f029731
Brian Olencki <brian@olencki.com>
2022-11-15 10:55:05 -0500

    Remove static method test

--- packages/utils/src/entity/createEntity.test.ts
+++ packages/utils/src/entity/createEntity.test.ts
@@ -6,11 +6,6 @@ describe('@codling/utils/entity/createEntity', function () {
     foo: z.literal('bar'),
   });
 
-  it('should create class with static properties', function () {
-    const Result = createEntity(schema);
-    this.assert.property(Result, 'from');
-  });
-
   it('should create instance of class', function () {
     const Result = createEntity(schema);
     const instance = new Result({ foo: 'bar' });
