7813d72e5b73096d641a4716a435a08278df7fa7
Brian Olencki <brian@olencki.com>
2022-12-16 18:52:29 -0500

    Process unknown errors into error instances

---
+++ packages/network/src/handleUnknownError.ts
@@ -0,0 +1,15 @@
+export function handleUnknownError(e: unknown): Error {
+  if (e instanceof Error) {
+    return e;
+  }
+
+  if (e && typeof e === 'object' && 'toString' in e) {
+    return new Error(e.toString());
+  }
+
+  if (typeof e === 'string') {
+    return new Error(e);
+  }
+
+  return new Error('Unknown error has occured.');
+}
